1dac62ec9842c5da72fd2d97ec904c24c6237429
huangjl1988 <645722942@qq.com>
2019-03-28 20:36:06 +0800

    首次递交

---
+++ src/main/java/com/huangjl/jquickness/TiggerApplication.java
@@ -0,0 +1,12 @@
+package com.huangjl.jquickness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiggerApplication.class, args);
+	}	
+}
